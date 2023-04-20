e4f751dc589272487b81dfc3abf7d4650e1ee5d4
Azul De Pol <azul.depol@gmail.com>
2023-04-20 22:20:17 +0100

    agregamos otro archivo de JS modulo

---
+++ js/administrador.js
@@ -0,0 +1,15 @@
+import Pelicula from "./classPelicula.js";
+
+const peliculaEjemplo = new Pelicula(
+    "0001",
+    "El Padrino",
+    "La familia Corleone es una de las más poderosas de Nueva York en los años 40.",
+    "https://imagenes.psicologiaymente.com/wp-content/uploads/2021/02/el-padrino-1.png",
+    "Drama/Crimen",
+    1972,
+    "2h 55min",
+    "Estados Unidos",
+    ["Marlon Brando", "Al Pacino", "James Caan"]
+  );
+
+  console.log(peliculaEjemplo);
